a1c211740434efcd09bb148323d263ba9ce9b7bc
Reggie Lee <reg@Reggies-Air.attlocal.net>
2022-05-06 19:51:24 -0500

    Created calculator container and buttons that still need to be arranged.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function add(num1, num2) {
+
+//basic equation functions
+function addition(num1, num2) {
     return Number(num1) + Number(num2)
 }
 
@@ -13,3 +15,22 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return Number(num1) / Number(num2)
 }
+
+function operate(operator, num1, num2) {
+    num1 = Number(num1)
+    num2 = Number(num2)
+    switch (operator) {
+        case '+':
+            return add(num1, num2)
+        case '-':
+            return substract(num1, num2)
+        case '*':
+            return multiply(num1, num2)
+        case '÷':
+            if (num2 === 0) return null
+            else return divide(num1, num2)
+        default:
+            return null
+    }
+}
+
